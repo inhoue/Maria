3a27438916efdd94bff6b034a61a88d6d494efea
inhoue <hinata44.135@gmail.com>
2012-03-11 12:59:22 +0100

    Push es projecte principal

---
+++ ProjecteGitHub/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Persona p = new Persona();
+		p.setNom("Maria");
+		p.setLlinatges("Mas Verdera");
+		p.setEdat("21");
+		p.setTelefon("666225533");
+		System.out.println(p.toString());
+	}
+}
